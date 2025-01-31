921e0c597c079f2c264883549b23c8aaf4a8fe0f
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2025-02-01 00:05:06 +0100

    user authentication

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -8,11 +10,21 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // Parse incoming requests: req.body
 app.use(cookieParser()); // Parse incoming cookies
 
 app.use("/api/auth", authRoutes);
+if (process.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
